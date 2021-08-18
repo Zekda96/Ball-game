6f634a1358a4dd9c26d31b6ab9e922119d867f85
Z96_mac <danielgranjac@gmail.com>
2021-08-18 16:18:44 -0500

    Reducing radius of Shape2D instead of scaling.

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -4,6 +4,7 @@ var Level
 var ball_scale
 var c
 var max_c
+var std_r = 128 # Shape size is made of radius of 128 pixels with scale of 0.5
 
 export var std_scale = 0.5
 var min_scale
@@ -23,10 +24,10 @@ func _process(delta):
 		max_c = float(Global.level_1_max_cascaritas)
 		print('Score = ', c, ' Goal: ', max_c)
 		ball_scale =  std_scale - ((std_scale - min_scale) * (c/max_c))
-		print('Scale by ', ball_scale)
+		print('Score = ', c, ' Goal: ', max_c, '. Scale by ', ball_scale)
 		$Sprite.scale = Vector2(ball_scale, ball_scale)
-		$CollisionShape2D.scale = Vector2(ball_scale, ball_scale)
-		$Area2D/CollisionShape2D.scale = Vector2(ball_scale, ball_scale)
+		$CollisionShape2D.get_shape().set_radius(ball_scale * std_r)
+		$Area2D/CollisionShape2D.get_shape().set_radius(ball_scale * std_r)
 
 
 	
